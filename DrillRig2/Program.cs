88e6a9fb3da18aed7af85ae1d9204addd67bcb20
Blomblomski <kevinplover@gmail.com>
2022-09-29 18:47:27 +0100

    Update Program.cs

--- DrillRig2/Program.cs
+++ DrillRig2/Program.cs
@@ -23,8 +23,8 @@ namespace IngameScript
     partial class Program : MyGridProgram
     {
         readonly List<IMyTerminalBlock> _drills = new List<IMyTerminalBlock>();
-        readonly List<IMyTerminalBlock> _verticalPistions = new List<IMyTerminalBlock>();
-        readonly List<IMyTerminalBlock> _horizontalPistons = new List<IMyTerminalBlock>();        
+        readonly List<IMyTerminalBlock> _verticalPistons = new List<IMyTerminalBlock>();
+        readonly List<IMyTerminalBlock> _horizontalPistons = new List<IMyTerminalBlock>();
         readonly List<IMyTerminalBlock> _motorRotor = new List<IMyTerminalBlock>();
 
         private enum RigStates
@@ -55,65 +55,69 @@ namespace IngameScript
         // rotation
         int NumberOfRotations { set; get; } // how many time the rig will rotate before one whole revolution 
         float StartingAngle { set; get; } // the position that the rig will start from.
-        float RoationAmountInDegrees { set; get; } // how much the rig will rotate per rotation.
+        float RotationAmountInDegrees { set; get; } // how much the rig will rotate per rotation.
 
         // pistions
-        float ExtendPistionVelocity { set; get; } // How fast the vertical booms extend.  More booms reduce the speed
-        float RetractPistionVelocity { set; get; } // How fast the booms will reset to default positions.
-        float MaxLimitDown { set; get; } // how far each pistion can extend
+        private float
+            ExtendPistonVelocity { set; get; } // How fast the vertical booms extend.  More booms reduce the speed
+
+        private float HorizontalPistonVelocity { set; get; }
+        float RetractPistonVelocity { set; get; } // How fast the booms will reset to default positions.
+        float MaxLimitDown { set; get; } // how far each piston can extend
         float PistionDownStartPosition { set; get; } // Set the starting extention of the pistions.
 
         // drills
         private bool DrillsEnabled { set; get; }
         private bool RigSetupComplete { set; get; }
 
+        private bool IsMaxDepth { set; get; }
+
+        private string test = "what";
+
         /// <summary>
         /// Set the default values for the rig.
         /// </summary>
         private void Reset()
         {
+            test = "reset";
             StartingAngle = 0.0f;
-            RoationAmountInDegrees = 15.0f;
-            NumberOfRotations = (int)(360 / RoationAmountInDegrees); // 360 / 15 = 24
+            RotationAmountInDegrees = 15.0f;
+            NumberOfRotations = (int)(360 / RotationAmountInDegrees); // 360 / 15 = 24
 
-            ExtendPistionVelocity = 0.3f;
-            RetractPistionVelocity = 2.0f;
+            ExtendPistonVelocity = 0.3f;
+            HorizontalPistonVelocity = 0.3f;
+            RetractPistonVelocity = 2.0f;
             MaxLimitDown = 2.0f; // At each drop extend the pistons by this value
-            PistionDownStartPosition = 0.0f; // How extended dp you need the pistions to be at the start
+            PistionDownStartPosition = 0.0f; // How extended you need the pistons to be at the start
 
-            if(PistionDownStartPosition > MaxLimitDown)
+            if (PistionDownStartPosition > MaxLimitDown)
             {
                 MaxLimitDown += PistionDownStartPosition;
             }
-            
+
+            IsMaxDepth = false;
             DrillsEnabled = false;
 
             _rigStates = RigStates.stop;
         }
-
 
         public Program()
         {
             Runtime.UpdateFrequency = UpdateFrequency.Update100;
 
             GridTerminalSystem.SearchBlocksOfName("Drill", _drills, drills => drills is IMyShipDrill);
-            GridTerminalSystem.SearchBlocksOfName("Vertical Piston", _verticalPistions, verticalPistions => verticalPistions is IMyPistonBase);
-            GridTerminalSystem.SearchBlocksOfName("Horizontal Piston", _horizontalPistons, horizontalPistons => horizontalPistons is IMyPistonBase);
-            GridTerminalSystem.SearchBlocksOfName("Motor Rotor", _motorRotor, motorRotor => motorRotor is IMyMotorStator);
-        }
-
-        public void Save()
-        {
-            // Called when the program needs to save its state. Use
-            // this method to save your state to the Storage field
-            // or some other means. 
-            // 
-            // This method is optional and can be removed if not
-            // needed.
+            GridTerminalSystem.SearchBlocksOfName("Vertical Piston", _verticalPistons,
+                verticalPistions => verticalPistions is IMyPistonBase);
+            GridTerminalSystem.SearchBlocksOfName("Horizontal Piston", _horizontalPistons,
+                horizontalPistons => horizontalPistons is IMyPistonBase);
+            GridTerminalSystem.SearchBlocksOfName("Motor Rotor", _motorRotor,
+                motorRotor => motorRotor is IMyMotorStator);
         }
 
         public void Main(string argument, UpdateType updateSource)
         {
+            Echo("Current Extenstion limit: " + MaxLimitDown + " " +test);
+            Echo("Current Hor velocity: " + HorizontalPistonVelocity);
             switch (_rigStates)
             {
                 case RigStates.setup:
@@ -124,30 +128,33 @@ namespace IngameScript
                     {
                         _rigStates = RigStates.start;
                     }
+
                     break;
                 case RigStates.reset:
                     Echo("Resetting the Rig.");
                     Reset();
-                    switch (argument)
-                    {
-                        case "setup":
-                            _rigStates = RigStates.setup;
-                            break;
-                        case "stop":
-                            _rigStates = RigStates.stop;
-                            break;
-                        default:
-                            Echo("Please enter setup or stop to proceed");
-                            break;
-                    }
                     break;
                 case RigStates.start:
                     Echo("The Rig is running.");
+                    DrillsEnabled = true;
+                    DrillSettings(DrillsEnabled);
                     Start();
                     break;
                 case RigStates.stop:
                     Echo("The Rig is stopped.");
                     RigStop();
+                    switch (argument)
+                    {
+                        case "setup":
+                            _rigStates = RigStates.setup;
+                            break;
+                        case "reset":
+                            _rigStates = RigStates.reset;
+                            break;
+                        default:
+                            Echo("Please enter setup or stop to proceed");
+                            break;
+                    }
                     break;
                 default:
                     _rigStates = RigStates.stop;
@@ -160,12 +167,16 @@ namespace IngameScript
             switch (_miningStates)
             {
                 case MiningStates.horizontal:
+                    Echo("Booms moving to target location");
+                    MiningHorizontal();
                     break;
                 case MiningStates.vertical:
                     Echo("Drill descending");
                     MiningVertical();
                     break;
                 case MiningStates.rotate:
+                    Echo("Rig rotating to target location");
+                    MiningRotate();
                     break;
                 default:
                     _miningStates = MiningStates.horizontal;
@@ -173,25 +184,47 @@ namespace IngameScript
             }
         }
 
+        private void MiningRotate()
+        {
+            foreach (var stator in _motorRotor.Cast<IMyMotorStator>())
+            {
+                stator.TargetVelocityRPM = ExtendPistonVelocity;
+
+                if (stator.Angle >= stator.UpperLimitRad)
+                {
+                    NumberOfRotations -= 1;
+                    _miningStates = MiningStates.horizontal;
+                    HorizontalPistonVelocity = HorizontalPistonVelocity * -1;
+                }
+            }
+        }
+
         private void MiningHorizontal()
         {
             foreach (var piston in _horizontalPistons.Cast<IMyPistonBase>())
             {
-                if (Math.Abs(piston.CurrentPosition - piston.HighestPosition) < 0.1f || Math.Abs(piston.CurrentPosition - piston.LowestPosition) < 0.01f)
+                piston.Velocity = HorizontalPistonVelocity;
+                Echo(" " + Math.Abs(piston.CurrentPosition - piston.HighestPosition));
+                
+                if (!(Math.Abs(piston.CurrentPosition - piston.HighestPosition) < 0.1f) && piston.Velocity > 0.0f ||
+                    !(Math.Abs(piston.CurrentPosition - piston.LowestPosition) < 0.1f) && piston.Velocity < 0.0f) continue;
+
+                piston.Velocity = 0.0f;
+                _miningStates = MiningStates.rotate;
+                foreach (var stator in _motorRotor.Cast<IMyMotorStator>())
                 {
-                    //piston.Velocity = piston.Velocity * -1;
-                    _miningStates = MiningStates.rotate;
-                    foreach (var stator in _motorRotor.Cast<IMyMotorStator>())
+                    if (NumberOfRotations > 0)
                     {
-                        if (NumberOfRotations > 0)
-                        { 
-                            stator.UpperLimitDeg += RoationAmountInDegrees;
-                        }
-                        else
+                        stator.UpperLimitDeg += RotationAmountInDegrees;
+                    }
+                    else
+                    {
+                        _miningStates = MiningStates.vertical;
+                        NumberOfRotations = (int)(StartingAngle / RotationAmountInDegrees);
+                        if (IsMaxDepth)
                         {
                             _rigStates = RigStates.stop;
                         }
-                        
                     }
                 }
             }
@@ -199,17 +232,22 @@ namespace IngameScript
 
         private void MiningVertical()
         {
-            foreach (var piston in _verticalPistions.Cast<IMyPistonBase>())
+            foreach (var piston in _verticalPistons.Cast<IMyPistonBase>())
             {
                 piston.MaxLimit = MaxLimitDown;
                 if (piston.CurrentPosition < MaxLimitDown)
                 {
-                    piston.Velocity = ExtendPistionVelocity;
+                    piston.Velocity = ExtendPistonVelocity;
                 }
                 else
                 {
                     piston.Velocity = 0.0f;
                     _miningStates = MiningStates.horizontal;
+                }
+
+                if (Math.Abs(piston.HighestPosition - piston.CurrentPosition) < 0.01f)
+                {
+                    IsMaxDepth = true;
                 }
             }
         }
@@ -225,7 +263,7 @@ namespace IngameScript
                 piston.Velocity = target;
             }
 
-            foreach (var piston in _verticalPistions.Cast<IMyPistonBase>())
+            foreach (var piston in _verticalPistons.Cast<IMyPistonBase>())
             {
                 piston.Velocity = target;
             }
@@ -240,16 +278,16 @@ namespace IngameScript
         {
             DrillSettings(DrillsEnabled);
 
-            foreach (var pistonBase in _verticalPistions.Cast<IMyPistonBase>())
+            foreach (var pistonBase in _verticalPistons.Cast<IMyPistonBase>())
             {
                 pistonBase.MinLimit = PistionDownStartPosition;
                 pistonBase.MaxLimit = PistionDownStartPosition;
 
-                if(pistonBase.CurrentPosition < PistionDownStartPosition)
+                if (pistonBase.CurrentPosition < PistionDownStartPosition)
                 {
                     pistonBase.Velocity = 0.5f;
                 }
-                else if(pistonBase.CurrentPosition > PistionDownStartPosition)
+                else if (pistonBase.CurrentPosition > PistionDownStartPosition)
                 {
                     pistonBase.Velocity = -0.5f;
                 }
@@ -261,7 +299,7 @@ namespace IngameScript
 
             foreach (var myPiston in _horizontalPistons.Cast<IMyPistonBase>())
             {
-                if(myPiston.CurrentPosition > 0.0f)
+                if (myPiston.CurrentPosition > 0.0f)
                 {
                     myPiston.Velocity = -0.5f;
                 }
@@ -278,7 +316,7 @@ namespace IngameScript
                 {
                     motorStator.TargetVelocityRPM = -0.5f;
                 }
-                else if(motorStator.Angle < motorStator.UpperLimitRad)
+                else if (motorStator.Angle < motorStator.UpperLimitRad)
                 {
                     motorStator.TargetVelocityRPM = 0.5f;
                 }
@@ -288,15 +326,15 @@ namespace IngameScript
                 }
             }
 
-            var vPiston = new List<IMyPistonBase>(_verticalPistions.Cast<IMyPistonBase>());
+            var vPiston = new List<IMyPistonBase>(_verticalPistons.Cast<IMyPistonBase>());
             var hPiston = new List<IMyPistonBase>(_horizontalPistons.Cast<IMyPistonBase>());
             var motor = new List<IMyMotorStator>(_motorRotor.Cast<IMyMotorStator>());
 
-            if(vPiston.All(o => o.Velocity == 0.0f) && hPiston.All(o => o.Velocity == 0.0f) && motor.All(o => o.TargetVelocityRPM == 0.0f))
+            if (vPiston.All(o => o.Velocity == 0.0f) && hPiston.All(o => o.Velocity == 0.0f) &&
+                motor.All(o => o.TargetVelocityRPM == 0.0f))
             {
                 RigSetupComplete = true;
             }
-
         }
 
         private void DrillSettings(bool status = false)
@@ -312,5 +350,6 @@ namespace IngameScript
         {
             return angle * (Pi / DAngle);
         }
+        
     }
 }
